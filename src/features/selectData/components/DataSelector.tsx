774560030dd1ddd00edcf4ada09720804ddb5082
Alex Kreidler <contributions@alexkreidler.com>
2022-01-30 02:50:15 -0500

    add data sources

--- src/features/selectData/components/DataSelector.tsx
+++ src/features/selectData/components/DataSelector.tsx
@@ -1,11 +1,25 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, HStack, Text, VStack } from "@chakra-ui/react";
 import React from "react";
+import { DataSource, DATA_SOURCES } from "./DataSource";
 
 export const DataSelector = () => {
   return (
-    <Box h="full" p={2}>
-      <Text fontWeight="bold">Choose datasets</Text>
-      Explainit
-    </Box>
+    <VStack h="full" p={2} alignItems="start">
+      <Text fontWeight="bold">Choose a data source</Text>
+      <Box
+        spacing={4}
+        p={4}
+        border="1px solid"
+        borderColor="gray.300"
+        borderRadius="md"
+        w="full"
+      >
+        <Text fontWeight="semibold">Local sources</Text>
+        <Text>CSV, XLS, JSON, or GeoJSON files.</Text>
+      </Box>
+      {DATA_SOURCES.map((d) => (
+        <DataSource source={d} />
+      ))}
+    </VStack>
   );
 };
