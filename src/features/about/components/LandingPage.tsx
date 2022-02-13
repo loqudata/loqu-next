0461debc8e45eb2d3f68053e8f1e1bf812295845
Alex Kreidler <contributions@alexkreidler.com>
2022-02-13 17:46:45 -0500

    update landing page

--- src/features/about/components/LandingPage.tsx
+++ src/features/about/components/LandingPage.tsx
@@ -7,19 +7,23 @@ import {
   Img,
   Stack,
   Image,
+  Center,
 } from "@chakra-ui/react";
 
 import React from "react";
 import { ChakraSearchBox } from "../../search/components/SearchBox";
 import { DynamicTypist } from "./Typer";
 
-import search from "assets/loqu_search.png";
+import { processImportedFile } from "utils/processImportedFile";
+
+import rawSearch from "assets/loqu_search.png";
+const searchImage = processImportedFile(rawSearch);
 
 import rawIcon from "assets/world_dots_grey.svg";
-// const rawIcon = await import("assets/visualize.svg");
+const worldIcon = processImportedFile(rawIcon);
 
-const worldIcon: string | { url: string } | { default: string } = rawIcon;
-console.log(worldIcon.default);
+import visualizeRaw from "assets/visualize.svg";
+const visualizeImage = processImportedFile(visualizeRaw);
 
 const hProps = {
   fontWeight: "regular",
@@ -27,54 +31,63 @@ const hProps = {
   lineHeight: "1.2",
 };
 
+function HeadlineSection() {
+  return (
+    <VStack spacing={6} alignItems="start" w="lg" mr={3}>
+      <VStack
+        color="gray.700" // fontFamily="Merriweather"
+        // justifyContent="center"
+        alignItems="start"
+      >
+        <Heading // as="span"
+          letterSpacing="tighter"
+          fontSize={{
+            base: "28",
+            md: "32",
+          }}
+          {...hProps}
+        >
+          Explore, search, and visualize
+        </Heading>
+        {/* Optional: */}
+        <Heading
+          display="flex"
+          flexDir="row"
+          flexWrap="nowrap"
+          fontSize={{
+            base: "48",
+            md: "64",
+          }}
+          {...hProps}
+        >
+          <DynamicTypist />
+          data
+        </Heading>
+      </VStack>
+      <Text // mt={"2rem !important"}
+        color="gray.700"
+        fontSize="24"
+        letterSpacing="tighter" // textAlign="center"
+        maxW="2lg"
+      >
+        Discover 700 million data series and 1.2 million datasets from hundreds
+        of official sources.
+      </Text>
+      {/* <worldIcon/> */}
+    </VStack>
+  );
+}
+
 export const LandingPage = () => {
   return (
-    <Box p={16}>
-      <Flex px={36} mb={20}>
-        <VStack spacing={6} alignItems="start" w="lg" mr={3}>
-          <VStack
-            color="gray.700"
-            // fontFamily="Merriweather"
-            // justifyContent="center"
-            alignItems="start"
-          >
-            <Heading
-              // as="span"
-              letterSpacing="tighter"
-              fontSize={{ base: "28", md: "32" }}
-              {...hProps}
-            >
-              Explore, search, and visualize
-            </Heading>
-            {/* Optional: */}
-            <Heading
-              display="flex"
-              flexDir="row"
-              flexWrap="nowrap"
-              fontSize={{ base: "48", md: "64" }}
-              {...hProps}
-            >
-              <DynamicTypist />
-              data
-            </Heading>
-          </VStack>
-          <Text
-            // mt={"2rem !important"}
-            color="gray.700"
-            fontSize="24"
-            letterSpacing="tighter"
-            // textAlign="center"
-            maxW="2lg"
-          >
-            Discover 700 million data series and 1.2 million datasets from hundreds of official sources.
-          </Text>
-          {/* <worldIcon/> */}
-        </VStack>
-        <Box flexGrow={1} />
-        <Box w="50%">
-          <Image src={worldIcon.default} />
+    <Box p={16} pt={0}>
+      <Center pt={20} pb={36}>
+        <HeadlineSection></HeadlineSection>
+        <Box w={16} />
+        <Box maxH="50vh" mt={-24}>
+          <Image boxSize="500px" src={visualizeImage} />
         </Box>
-      </Flex>
+      </Center>
 
       <Flex justifyContent="center">
         <Box w="md" mr={8}>
@@ -86,18 +99,25 @@ export const LandingPage = () => {
             Search
           </Heading>
           <Text
-          mt={3}
+            mt={3}
             color="gray.700"
             fontSize={24}
             letterSpacing="tighter"
             maxW="2lg"
           >
-            Filter and facet datasets by format, update frequency, data created, or license.
+            Filter and facet datasets by format, update frequency, data created,
+            or license.
           </Text>
         </Box>
 
-        <Box w="60%" borderRadius="md" border="1px solid" borderColor="gray.300" p={2}>
-          <Image src={search as any}></Image>
+        <Box
+          w="60%"
+          borderRadius="md"
+          border="1px solid"
+          borderColor="gray.300"
+          p={2}
+        >
+          <Image src={searchImage}></Image>
         </Box>
       </Flex>
     </Box>
