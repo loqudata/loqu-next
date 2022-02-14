ebeccb2af38229a9adeba05a32ac50bcc573fffe
Alex Kreidler <contributions@alexkreidler.com>
2022-02-13 20:45:29 -0500

    make features component

--- src/features/about/components/LandingPage.tsx
+++ src/features/about/components/LandingPage.tsx
@@ -9,6 +9,7 @@ import {
   Image,
   Center,
   HeadingProps,
+  HStack,
 } from "@chakra-ui/react";
 
 import React from "react";
@@ -37,7 +38,7 @@ const subHeadingSize = { base: "regular", md: "xl" };
 
 function HeadlineSection() {
   return (
-    <VStack spacing={6} alignItems="start" w="lg" mr={{base: -16, md: 3}} >
+    <VStack spacing={6} alignItems="start" w="lg" mr={{ base: -16, md: 3 }}>
       <VStack color="gray.700" alignItems="start">
         <Heading {...hProps} fontSize={headingLargeSize}>
           Explore, search, and visualize
@@ -49,57 +50,127 @@ function HeadlineSection() {
           // size="3xl"
           fontSize={{ base: "5xl", md: "6xl" }}
           maxW="40vw"
-          flexWrap={{base: "wrap", lg: "nowrap"}}
+          flexWrap={{ base: "wrap", lg: "nowrap" }}
         >
           <DynamicTypist />
           data
         </Heading>
       </VStack>
-      <Text {...hProps} fontSize={subHeadingSize} w="fit-content" maxW={{base: "sm", md: "2lg"}}>
+      <Text
+        {...hProps}
+        fontSize={subHeadingSize}
+        w="fit-content"
+        maxW={{ base: "sm", md: "2lg" }}
+      >
         Discover 700 million data series and 1.2 million datasets from hundreds
         of official sources.
       </Text>
     </VStack>
   );
 }
+const features = [
+  {
+    name: "Visualize",
+    description: [
+      "Easily build charts that map field data to visual properties like color, size, shape, or the X and Y axes.",
+      "Explore the data quicker, and get inspired with recommended charts.",
+    ],
+    image: searchImage,
+  },
+  {
+    name: "Search",
+    description:
+      "Filter and facet datasets by file format, license, date, or update frequency.",
+    image: searchImage,
+  },
+];
 
-export const LandingPage = () => {
-  return (
-    <Box p={{base: 4, md: 8, lg: 16}} pt={0}>
-      <Flex justifyContent="center" alignItems="center" pt={20} pb={{ base: 16, lg: 36 }} flexDir={{base: "column", md: "row"}} gap={10}>
-        <HeadlineSection></HeadlineSection>
-        <Box mt={null}>
-          <Image maxH="50vh" maxW="50vw" src={visualizeImage} />
-        </Box>
-      </Flex>
-
-      <Flex justifyContent="center" flexDir={{base: "column", md: "row"}} gap={3}>
-        <Box w="md" mr={8}>
-          <Heading {...hProps} fontSize={headingLargeSize}>
-            Search
-          </Heading>
+function Feature({
+  name,
+  description,
+  image,
+  textSide,
+}: { textSide: "left" | "right" } & typeof features[0]) {
+  const text = (
+    <Box w="sm">
+      <Heading {...hProps} fontWeight="bold" letterSpacing="tight" fontSize={headingLargeSize}>
+        {name}
+      </Heading>
+      {Array.isArray(description) ? (
+        description.map((el) => (
           <Text
             mt={3}
             color="gray.700"
             fontSize={subHeadingSize}
             letterSpacing="tighter"
-            maxW="2lg"
           >
-            Filter and facet datasets by format, update frequency, data created,
-            or license.
+            {el}
           </Text>
-        </Box>
-
-        <Box
-          w={{base: "fit-content", md: "60%"}}
-          borderRadius="md"
-          border="1px solid"
-          borderColor="gray.300"
-          p={2}
+        ))
+      ) : (
+        <Text
+          mt={3}
+          color="gray.700"
+          fontSize={subHeadingSize}
+          letterSpacing="tighter"
         >
-          <Image src={searchImage}></Image>
+          {description}
+        </Text>
+      )}
+    </Box>
+  );
+  const visual = (
+    <Box
+      w={{
+        base: "fit-content",
+        xl: "60%",
+      }}
+      borderRadius="md"
+      border="1px solid"
+      borderColor="gray.300"
+      p={2}
+    >
+      <Image src={image}></Image>
+    </Box>
+  );
+  return (
+
+    <HStack
+      justifyContent="center"
+      // alignItems="start"
+      spacing={12}
+      flexDir={{
+        base: "column",
+        xl: "row",
+      }}
+      gap={3}
+    >
+      {textSide == "left" ? [text, visual] : [visual, text]}
+    </HStack>
+  );
+}
+
+export const LandingPage = () => {
+  return (
+    <Box p={{ base: 4, md: 8, lg: 16 }} pt={0}>
+      <Flex
+        justifyContent="center"
+        alignItems="center"
+        pt={20}
+        pb={{ base: 16, lg: 36 }}
+        flexDir={{ base: "column", md: "row" }}
+        gap={10}
+      >
+        <HeadlineSection></HeadlineSection>
+        <Box mt={null}>
+          <Image maxH="50vh" maxW="50vw" src={visualizeImage} />
         </Box>
       </Flex>
+      <VStack spacing={28}>
+        {features.map((f, i) => (
+          <Feature textSide={i % 2 == 0 ? "left" : "right"} {...f}></Feature>
+        ))}
+      </VStack>
     </Box>
   );
 };
